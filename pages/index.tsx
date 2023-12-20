b09d37d280b4cd48fb95fafc80cf5ed7da791290
naelengithub <anaelisa@mailmasker.com>
2023-12-20 16:41:06 -0600

    feat: stary tuned

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,31 @@
 import { Footer } from "../src/components/footer/footer";
 import { NavBar } from "../src/components/navbar/navbar";
 import Head from "next/head";
-import P5Sketch from "../src/components/p5-sketch";
+import dynamic from "next/dynamic";
+
+// Import P5Sketch dynamically
+const DynamicP5Sketch = dynamic(() => import("../src/components/p5-sketch"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-orange-50 flex flex-col">
+    <div className="min-h-screen flex relative flex-col">
       <Head>
         <title>Ana Elisa Vargas</title>
       </Head>
       {/* <NavBar /> */}
-      <div className="pt-8 text-center gap-4">
+      <div className="pt-8 text-center gap-4 z-[100]">
         <h2>Stay tuned...</h2>
         <h3>
           This site is under renovation <br />
           Feb 2024
         </h3>
       </div>
-      <div className="sketch">{/* <P5Sketch /> */}</div>
+      <div className="absolute">
+        {/* Render the DynamicP5Sketch component */}
+        <DynamicP5Sketch />
+      </div>
       {/* <Footer /> */}
     </div>
   );
--- src/components/p5-sketch.js
+++ src/components/p5-sketch.js
@@ -1,43 +1,125 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Sketch from "react-p5";
 
 function P5Sketch() {
-  const [speed, setSpeed] = useState(2);
-  const [x, setX] = useState(50);
-
-  useEffect(() => {
-    const handleAnimation = () => {
-      // Animation logic here
-      if (speed > 0) {
-        if (x + 50 < 500) {
-          setX(x + speed);
-        } else {
-          setSpeed(-speed);
-        }
-      } else {
-        if (x - 50 > 0) {
-          setX(x + speed);
-        } else {
-          setSpeed(-speed);
-        }
-      }
-    };
-
-    const intervalId = setInterval(handleAnimation, 1000 / 30); // 30 frames per second
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [x, speed]);
+  const numBalls = 13;
+  const spring = 0.05;
+  const gravity = 0.03;
+  const friction = -0.9;
+  const balls = [];
 
   const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(500, 400).parent(canvasParentRef);
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    p5.createCanvas(windowWidth, windowHeight).parent(canvasParentRef);
+
+    for (let i = 0; i < numBalls; i++) {
+      balls[i] = new Ball(
+        p5.random(windowWidth),
+        p5.random(windowHeight),
+        p5.random(30, 70),
+        i,
+        balls
+      );
+    }
+    p5.noStroke();
+    p5.fill(0);
   };
 
   const draw = (p5) => {
-    p5.background(255, 120, 20);
-    p5.ellipse(x, 100, 100);
+    p5.background(255, 247, 237);
+    balls.forEach((ball) => {
+      ball.collide();
+      ball.move(p5); // Pass the p5 object
+      ball.display(p5);
+    });
   };
+
+  const windowResized = (p5) => {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    p5.resizeCanvas(windowWidth, windowHeight);
+
+    balls.forEach((ball) => {
+      ball.x = p5.random(windowWidth);
+      ball.y = p5.random(windowHeight);
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", windowResized);
+    return () => {
+      window.removeEventListener("resize", windowResized);
+    };
+  }, []);
+
+  class Ball {
+    constructor(xin, yin, din, idin, oin) {
+      this.x = xin;
+      this.y = yin;
+      this.vx = 0;
+      this.vy = 0;
+      this.diameter = din;
+      this.id = idin;
+      this.others = oin;
+    }
+
+    collide() {
+      for (let i = this.id + 1; i < numBalls; i++) {
+        let dx = this.others[i].x - this.x;
+        let dy = this.others[i].y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+
+        if (distance < minDist) {
+          let angle = Math.atan2(dy, dx);
+          let targetX = this.x + Math.cos(angle) * minDist;
+          let targetY = this.y + Math.sin(angle) * minDist;
+          let ax = (targetX - this.others[i].x) * spring;
+          let ay = (targetY - this.others[i].y) * spring;
+          this.vx -= ax;
+          this.vy -= ay;
+          this.others[i].vx += ax;
+          this.others[i].vy += ay;
+        }
+      }
+    }
+
+    move(p5) {
+      this.vy += gravity;
+      this.x += this.vx;
+      this.y += this.vy;
+
+      const canvasWidth = p5.width;
+      const canvasHeight = p5.height;
+
+      if (this.x + this.diameter / 2 > canvasWidth) {
+        this.x = canvasWidth - this.diameter / 2;
+        this.vx *= friction;
+      } else if (this.x - this.diameter / 2 < 0) {
+        this.x = this.diameter / 2;
+        this.vx *= friction;
+      }
+
+      if (this.y + this.diameter / 2 > canvasHeight) {
+        this.y = canvasHeight - this.diameter / 2;
+        this.vy *= friction;
+      } else if (this.y - this.diameter / 2 < 0) {
+        this.y = this.diameter / 2;
+        this.vy *= friction;
+      }
+    }
+
+    display(p5) {
+      p5.ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+
+    display(p5) {
+      p5.ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+  }
 
   return <Sketch setup={setup} draw={draw} />;
 }
