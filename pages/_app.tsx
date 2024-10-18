7b20bf18f76e35d62549a9f3d35733191442950d
naelengithub <anaelisa@mailmasker.com>
2024-10-18 15:29:03 +0200

    feat: added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
+      <Head>
+        {/* You can also add larger icons or different formats */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon.png"
+        />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <title>Ana Elisa</title> {/* You can also set a custom title */}
+      </Head>
       <Component {...pageProps} />
     </RecoilRoot>
   );
