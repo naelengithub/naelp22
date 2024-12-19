34144c7a8aed3cb22fc3fee02c1febfc1871bec0
naelengithub <anaelisa@mailmasker.com>
2024-12-19 10:45:31 +0100

    update: about text

--- pages/about.tsx
+++ pages/about.tsx
@@ -36,41 +36,43 @@ export default function Contact() {
           <div className="mt-20 ml-28 mb-14">
             <p>
               <span className="text-4xl">
-                <DynamicShadowText text="ana elisa" />
+                <DynamicShadowText text="Hi." />
               </span>
               <span>
-                is a seasoned project developer and designer with a presence in
-                Berlin, Mexico City, and San Francisco. Although initially
-                trained as an architect, she has successfully transitioned into
-                leading a diverse range of projects, including the production of
-                a short film, alongside her extensive work in web design and
-                development.
+                I&apos;m Ana Elisa, a digital project designer and developer
+                with a presence between Mexico City and Berlin.
               </span>
             </p>
             <p className="mt-4">
-              In addition to her freelance expertise in design and Front-End
-              development{" "}
-              <strong>(Typescript, React, low/no-code platforms)</strong>, she
-              excels in operating end-to-end client projects. From the
-              conceptualization phase to delivering fully functional, live
-              websites, she offers a comprehensive, hands-on approach that
-              ensures every project is executed to the highest standard. This
-              website serves as a prime example of her ability to design,
-              develop, and launch integrated digital experiences that are both
-              aesthetically compelling and highly functional.
+              With extensive freelance experience in web design and front-end
+              development (Typescript, React, low/no-code platforms), I
+              specialize in managing end-to-end client projects—from initial
+              concept to the launch of{" "}
+              <strong>
+                fully functional, live websites and ecommerce stores
+              </strong>
+              . My approach is hands-on and comprehensive, ensuring digital
+              experiences that are are both aesthetically compelling and highly
+              functional.
             </p>
             <p className="mt-4">
-              With experience in designing complex data flows, automation, and
-              system integration using API calls and webhooks, she provides
-              tailored, scalable solutions that enable businesses to excel in
-              the digital space. Her expertise is especially valuable for
-              <strong>startups seeking scalable solutions</strong> to support
-              their growth and for entrepreneurs seeking to digitalize their
-              products and/or services.
+              I believe creating tailored, scalable solutions empower small
+              businesses to excel in the digital space, because businesses
+              thrive when their digital strategies are customized to meet the
+              unique needs of their target market.
             </p>
             <p className="mt-4">
-              Ana Elisa is driven by projects that inspire and motivate their
-              teams, believing the most fulfilling work aligns with both
+              I tend to work with{" "}
+              <strong>
+                entrepreneurs seeking to digitalize their products/services{" "}
+              </strong>
+              and startups seeking scalable solutions to support their growth.
+              Does that sound like you? Hit me up, I am always up to chatting
+              about collaborations.
+            </p>
+            <p className="mt-4">
+              Above all, I am driven by projects that inspire and motivate their
+              creators, believing the most fulfilling work aligns with both
               business goals and a deeper sense of purpose or social impact.
             </p>
           </div>
