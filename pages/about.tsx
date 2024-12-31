85c9e146f276e9bc52b39876372de1def963231e
naelengithub <anaelisa@mailmasker.com>
2024-12-31 01:53:03 +0100

    feat: added exp and education to about

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { useWindowSize } from "../src/hooks/useWindowSize";
 import DynamicShadowText from "../src/components/DynamicShadowText";
 import Link from "next/link";
+import { jobExperience } from "../src/components/web/experience";
+import { education } from "../src/components/web/education";
 
 export default function Contact() {
   const size = useWindowSize();
@@ -22,7 +24,7 @@ export default function Contact() {
         made by urs truly, with &hearts; in Berlin © 2024
       </p>
       <div>
-        <div className="block pt-80 w-1/4 p-4 min-w-[360px] md:m-auto">
+        <div className="block pt-80 w-1/3 p-4 min-w-[360px] md:m-auto">
           <div className="float-left w-48 mr-4">
             <Link href="/">
               <Image
@@ -39,44 +41,72 @@ export default function Contact() {
                 <DynamicShadowText text="Hi." />
               </span>
               <span>
-                I&apos;m Ana Elisa, a digital project designer and developer
-                with a presence between Mexico City and Berlin.
+                I&apos;m Ana Elisa, a hands-on digital project designer and
+                developer with a proven track record of 4+years, specializing in
+                managing and operating end-to-end client projects—from initial
+                concept to the launch of fully functional, live websites and
+                ecommerce stores.
+              </span>
+              <span>
+                &nbsp;My approach ensures digital experiences that are are both
+                aesthetically compelling and highly functional.
               </span>
             </p>
             <p className="mt-4">
-              With extensive freelance experience in web design and front-end
-              development (Typescript, React, low/no-code platforms), I
-              specialize in managing end-to-end client projects—from initial
-              concept to the launch of{" "}
-              <strong>
-                fully functional, live websites and ecommerce stores
-              </strong>
-              . My approach is hands-on and comprehensive, ensuring digital
-              experiences that are are both aesthetically compelling and highly
-              functional.
+              I’m a self-starter with a knack for adapting and problem-solving,
+              honed through my experiences as a third-culture kid. I design and
+              develop digital products that not only scale businesses but also
+              help transmit their message and evoke the right emotions. My role
+              spans across operations, from setting up new projects and
+              responding to briefs and RFPs to co-creating pitches, treatments,
+              and proposals. I handle the entire project lifecycle, from design
+              and development to scoping, quoting, and planning, ensuring
+              everything runs smoothly, is profitable, and meets deadlines.
             </p>
             <p className="mt-4">
-              I believe creating tailored, scalable solutions empower small
-              businesses to excel in the digital space, because businesses
-              thrive when their digital strategies are customized to meet the
-              unique needs of their target market.
+              I mostly work with entrepreneurs digitizing their products and
+              startups scaling for growth (though I somehow ended up producing a
+              short film this summer—who knew?). Does that sound like you? Hit
+              me up, I am always up to chatting about collaborations.
             </p>
             <p className="mt-4">
-              I tend to work with{" "}
-              <strong>
-                entrepreneurs seeking to digitalize their products/services{" "}
-              </strong>
-              and startups seeking scalable solutions to support their growth.
-              Does that sound like you? Hit me up, I am always up to chatting
-              about collaborations.
-            </p>
-            <p className="mt-4">
-              Above all, I am driven by projects that inspire and motivate their
-              creators, believing the most fulfilling work aligns with both
-              business goals and a deeper sense of purpose or social impact.
+              Side note - I am driven by projects that inspire and motivate
+              their creators, believing the most fulfilling work aligns with
+              both business goals and a deeper sense of purpose or social
+              impact.
             </p>
           </div>
         </div>
+        <section className="flex flex-col p-4 min-w-[360px] m-auto w-2/3 md:w-5/6">
+          <span className="text-4xl md:ml-52">
+            <DynamicShadowText text="Experience" />
+          </span>
+          {jobExperience.map((job, index) => (
+            <div key={index} className="flex justify-center gap-6 mt-12">
+              <div className="flex-col w-36 md:w-48">
+                <h3>
+                  {job.title} - {job.company}, {job.location}
+                </h3>
+                <p>{job.year}</p>
+              </div>
+              <p className="w-1/2 md:w-1/3">{job.description}</p>
+            </div>
+          ))}
+        </section>
+        <section className="flex flex-col p-4 min-w-[360px] mx-auto mt-20 mb-40 w-2/3 md:w-5/6">
+          <span className="text-4xl md:ml-52">
+            <DynamicShadowText text="Education" />
+          </span>
+          {education.map((school, index) => (
+            <div key={index} className="flex justify-center gap-6 mt-12">
+              <div className="flex-col w-36 md:w-48">
+                <h3>{school.title}</h3>
+                <p>{school.year}</p>
+              </div>
+              <p className="w-1/2 md:w-1/3">{school.description}</p>
+            </div>
+          ))}
+        </section>
         <button
           onClick={() => {
             window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
---
+++ src/components/web/education.ts
@@ -0,0 +1,23 @@
+export const education = [
+  {
+    title: "Diploma: Marketing and Digital Strategy",
+    sede: "FAD, UNAM",
+    year: "Aug 2024 - Jan 2025",
+    description:
+      "Relevant coursework: Storytelling, Digital Mindset and User Experience Design, Digital Strategies and Social Media Content Creation.",
+  },
+  {
+    title: "Bootcamp in Front-End Engineering",
+    sede: "Hackademy",
+    year: "Jan 2022 - Aug 2022",
+    description:
+      "Relevant coursework includes JavaScript, React, HTML/CSS, responsive design, UI/UX principles, version control with Git, and foundational computer science topics such as algorithms, data structures, and web performance optimization.",
+  },
+  {
+    title: "B.A. Architecture (5-year program)",
+    sede: "FA, UNAM",
+    year: "2017 - 2022",
+    description:
+      "Final two years completed in an exchange with the M.A. in Architecture program at Technische Universität Berlin, Germany (2020-2022).",
+  },
+];
